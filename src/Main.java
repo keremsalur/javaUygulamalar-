c7c78abb3263046ab9231bd0acd3b2462befef64
Kerem Salur <keremsalur@gmail.com>
2024-02-08 12:57:57 +0300

    manav kasa fiyat hesaplama

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
-        int kilo;
-        float vki,boy;
+        float armut, elma, domates, muz, patlican, toplam;
         Scanner input = new Scanner(System.in);
-        System.out.print("Boy (m) : ");
-        boy = input.nextFloat();
-        System.out.print("Kilo: ");
-        kilo = input.nextInt();
-        vki = (float) kilo * boy / boy;
+        System.out.print("Armut kaç kilo : ");
+        armut = input.nextFloat();
+        System.out.print("Elma kaç kilo : ");
+        elma = input.nextFloat();
+        System.out.print("Domates kaç kilo : ");
+        domates = input.nextFloat();
+        System.out.print("Muz kaç kilo : ");
+        muz = input.nextFloat();
+        System.out.print("Patlican kaç kilo : ");
+        patlican = input.nextFloat();
 
-        System.out.println("Vücut kitle indexi: " + vki);
+        toplam = (armut * 2.14f) + (elma * 3.67f) + (domates * 1.1f) + (muz * 0.95f) + (patlican * 5.0f);
 
-
+        System.out.println("Toplam: " + toplam);
 
     }
 }
