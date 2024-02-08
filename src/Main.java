d9cfb3aa888928278dd576e90214193c3e13fdb6
Kerem Salur <keremsalur@gmail.com>
2024-02-08 12:35:48 +0300

    üçgenin çevresinden alanı hesaplama

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         // 6
-        int urunFiyati;
+        int a,b,c;
+        float u,alan;
         Scanner input = new Scanner(System.in);
-        System.out.print("Ürün Fiyatını Giriniz: ");
-        urunFiyati = input.nextInt();
-        float kdvliFiyat = urunFiyati * ((urunFiyati <= 1000)?1.18f:1.08f);
-        System.out.println("Kdvli ürün fiyatı: " + kdvliFiyat);
+        System.out.print("A kenarını girin: ");
+        a = input.nextInt();
+        System.out.print("B kenarını girin: ");
+        b = input.nextInt();
+        System.out.print("C kenarını girin: ");
+        c = input.nextInt();
+        u = (a + b + c) / 2.0f;
+        alan = u * (u - a) * (u - b) * (u - c);
+        alan = (float) Math.sqrt(alan);
+
+        System.out.println("Üçgenin alanı: " + alan);
 
 
 
